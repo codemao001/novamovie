d3a2b9c6f60764adfb7b210c4c8d5d6ab601a5fa
yolanday <yuenlanda@gmail.com>
2015-12-08 13:11:04 +0800

    test

--- app.js
+++ app.js
@@ -1,47 +1,44 @@
 var express = require('express')
 var port = process.env.PORT || 8080
 var path = require('path')
+var mongoose = require('mongoose')
+var _ = require('underscore')
+var Movie = require('./models/movie')
 var app = express()
-var session = require('express-session');
+var session = require('express-session')
+var bodyParser = require('body-parser');
+
+
+mongoose.connect('mongodb://localhost/nova')
 
 app.set('views', './views/pages')
 app.set('view engine','jade')
 app.use(session({secret: 'keyboard cat'}))
-// app.use(express.bodyParser())
+app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'bower_components')))
+app.locals.moment = require('moment')
 app.listen(port)
 
 console.log('novamovie started on port' + port)
 
 // index page
 app.get('/', function(req,res){
-	res.render('index',{
-		title:'movieNova 首页',
-		movies: [{
-			title: '机械战警',
-			_id: 1,
-			poster: "http://r3.ykimg.com/05160000530EEB63675839160D0B79D5"
-		},{
-			title: '机械战警',
-			_id: 2,
-			poster: "http://r3.ykimg.com/05160000530EEB63675839160D0B79D5"
-		}]
+	Movie.fetch(function(err, movie){//TODO: DRY2
+		if(err) {
+			console.log(err)
+
+		}
+		res.render('index', { title: 'nova Homepage', movie:movie })
 	})
+
 })
 
 // detail page
 app.get('/movie/:id',function(req,res){
-	res.render('detail',{
-		title:'movieNova 详情页',
-		movies: {
-			title : "机械战警",
-			doctor : "和塞",
-			country : "美国",
-			language : "英语",
-			poster : "http://r3.ykimg.com/05160000530EEB63675839160D0B79D5",
-			summary : "《机械战警》是由何塞·帕迪里亚执导，乔尔·金纳曼、塞缪尔·杰克逊、加里·奥德曼等主演的一部科幻电影，改编自1987年保罗·范霍文执导的同名电影。影片于2014年2月12日在美国上映，2014年2月28日在中国大陆上映。影片的故事背景与原版基本相同，故事设定在2028年的底特律，男主角亚历克斯·墨菲是一名正直的警察，被坏人安装在车上的炸弹炸成重伤，为了救他，OmniCorp公司将他改造成了生化机器人“机器战警”，代表着美国司法的未来。",
-			flash : "http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf"
-		}
+	var id=req.params.id
+	Movie.findById(id, function(err, movie){
+		if(err){ console.log(err) }
+		res.render('detail', {title:'nova '+movie.title, movie:movie});
 	})
 })
 
@@ -49,7 +46,7 @@ app.get('/movie/:id',function(req,res){
 app.get('/admin/movie',function(req,res){
 	res.render('admin',{
 		title:'movieNova 后台录入页',
-		movies: {
+		movie: {
 			title : '',
 			doctor : '',
 			country : '',
@@ -62,20 +59,79 @@ app.get('/admin/movie',function(req,res){
 	})
 })
 
+//admin update movie
+app.get('/admin/update/:id', function(req, res){
+	var id=req.params.id
+	if(id){
+		Movie.findById(id, function(err, movie){
+			res.render('admin',{ title:'nova adminUpdatePage', movie:movie})
+		})
+	}
+})
+
+app.get('/tool/movie_save2', function(req,res){
+	var o = new Movie({doctor:'kkkk'})
+	o.save()
+	res.render('debug')
+})
+
+app.get('/tool/movie_save', function(req,res){
+	var var1=print_r({k1:'k1v',k2:'k2v', k3:'k3v'})
+	res.render('debug', {var1:var1})
+})
+
+// admin post movie
+app.post('/admin/movie/new',function(req,res){
+	console.log(req.query);
+	console.log(req.body);
+	var id = req.body.movie._id
+	var movieObj = req.body.movie
+
+	var _movie
+
+	if(id !=='undefined'){
+		Movie.findById(id,function(err,movie){
+			if(err){
+				console.log(err)
+			}
+
+			_movie = _.extend(movie,movieObj)
+			_movie.save(function(err,movie){
+				if(err){
+					console.log(err)
+				}
+				res.redirect('/movie/'+ movie._id)
+			})
+		})
+	}else{
+		_movie = new Movie({
+			doctor:movieObj.doctor,
+			title:movieObj.title,
+			country:movieObj.country,
+			language:movieObj.language,
+			year:movieObj.year,
+			poster:movieObj.poster,
+			summary:movieObj.summary,
+			flash:movieObj.flash
+		})
+
+		_movie.save(function(err,movie){
+			if(err){
+				console.log(err)
+			}
+
+			res.redirect('/movie/'+movie._id)
+		})
+	}
+})
+
 // list page
 app.get('/admin/list',function(req,res){
-	res.render('list',{
-		title:'movieNova 列表页',
-		movies: [{
-			title: '机械战警',
-			_id: 1,
-			doctor : "和塞",
-			country : "美国",
-			year: 2014,
-			poster: "http://r3.ykimg.com/05160000530EEB63675839160D0B79D5",
-			language: '英语',
-			summary : "《机械战警》是由何塞·帕迪里亚执导，乔尔·金纳曼、塞缪尔·杰克逊、加里·奥德曼等主演的一部科幻电影，改编自1987年保罗·范霍文执导的同名电影。影片于2014年2月12日在美国上映，2014年2月28日在中国大陆上映。影片的故事背景与原版基本相同，故事设定在2028年的底特律，男主角亚历克斯·墨菲是一名正直的警察，被坏人安装在车上的炸弹炸成重伤，为了救他，OmniCorp公司将他改造成了生化机器人“机器战警”，代表着美国司法的未来。",
-		 	flash : "http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf"
-		}]
+	Movie.fetch(function(err, movie){//TODO:DRY2
+		if(err){
+			console.log(err)
+		}
+
+		res.render('list', { title: 'nova listPage', movie:movie })
 	})
 })
